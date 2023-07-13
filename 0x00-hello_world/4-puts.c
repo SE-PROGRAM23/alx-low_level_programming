bb5d36f07d623ab6f7c629c6a0bd9e9d16bf5601
oussamabougad <oussamabougad801@gmail.com>
2023-07-13 14:32:06 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+//File: 4-puts.c
+#include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
